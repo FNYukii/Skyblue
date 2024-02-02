0edb74f6a27b617d7d8c16825bbfdb7c3d7b28ba
FNYukii <forestgreen14689@gmail.com>
2024-02-02 19:14:59 +0900

    feat: PickImageButton

--- src/views/screens/AddScreen.tsx
+++ src/views/screens/AddScreen.tsx
@@ -1,12 +1,29 @@
+import { useState } from "react"
 import URLModal from "../components/URLModal"
+import PickImagesButton from "../components/PickImagesButton"
 
 function AddScreen() {
+
+	const [images, setImages] = useState<File[]>([])
+	const [title, setTitle] = useState("")
+	const [detail, setDetail] = useState("")
+
+
 
 	return (
 
 		<URLModal>
 
-			<p className="">Add Screen here.</p>
+			<h1 className="text-2xl font-bold">新しいスポット</h1>
+
+			<PickImagesButton onPick={(images) => setImages(images)} className="mt-4" />
+
+			<div className="mt-4   grid grid-cols-2 gap-2">
+
+				{images.map((photo) => (
+					<img src={window.URL.createObjectURL(photo)} alt="Images for new Spot" className="h-32 aspect-square rounded-xl" />
+				))}
+			</div>
 		</URLModal>
 	)
 }
