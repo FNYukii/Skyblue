208ac8bb4d5e16339646290989e7e9c191b43298
FNYukii <forestgreen14689@gmail.com>
2024-02-02 16:08:33 +0900

    feat: Firebase初期化ファイル作成

---
+++ src/utils/firebase.ts
@@ -0,0 +1,24 @@
+import { initializeApp } from "firebase/app"
+import { getFirestore } from "firebase/firestore"
+import { getAuth } from "firebase/auth"
+
+// .envファイルに書いてある環境変数を元に、Firebaseプロジェクトの構成情報をまとめる
+const firebaseConfig = {
+	apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+	authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+	databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+	projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+	storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.REACT_APP_FIREBASE_APP_ID,
+	measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+}
+
+// 構成情報を元に、Firebaseを初期化
+initializeApp(firebaseConfig)
+
+// AuthenticationとCloud Firestoreへの参照を取得
+const auth = getAuth()
+const db = getFirestore()
+
+export { auth, db }
