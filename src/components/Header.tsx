c6fd1e53b800d3e910e97ccd91d47bfb382c488d
FNYukii <forestgreen14689@gmail.com>
2024-02-02 16:21:01 +0900

    feat: Googleサインインを実装

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,55 @@
 import { NavLink } from "react-router-dom"
+import AuthService from "../utils/AuthService"
+import { onAuthStateChanged } from "firebase/auth"
+import { useState, useEffect } from "react"
+import { auth } from "../utils/firebase"
 
 function Header() {
+
+
+
+	// サインインしているかどうか
+	const [isSignedIn, setIsSignedIn] = useState(false)
+	const [isLoaded, setIsLoaded] = useState(false)
+
+
+
+	useEffect(() => {
+
+		// ログイン状態を取得
+		onAuthStateChanged(auth, (user) => {
+			if (user) {
+				// ログイン済み
+				setIsSignedIn(true)
+			} else {
+
+				// 未ログイン
+				setIsSignedIn(false)
+			}
+
+			setIsLoaded(true)
+		})
+	}, [])
+
+
 
 	return (
 
 		<header className="mx-auto   w-full lg:w-[1024px]   px-4 lg:px-0   py-4   flex justify-between items-center">
 
 			<NavLink to="/" className="text-3xl font-light">Skyblue</NavLink>
-			<NavLink to="/account" className="hover:underline">Account</NavLink>
+
+			{!isLoaded &&
+				<p>Loading...</p>
+			}
+
+			{isLoaded && !isSignedIn &&
+				<button onClick={() => AuthService.signInWithGoogle()} className="hover:underline">Sign In</button>
+			}
+
+			{isLoaded && isSignedIn &&
+				<button onClick={() => AuthService.signOut()} className="hover:underline">Sign Out</button>
+			}
 		</header>
 	)
 }
