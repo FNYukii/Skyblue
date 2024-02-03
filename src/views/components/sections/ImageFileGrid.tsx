defa3f8d955c3c5ce172a23ba89e9fb978a2c4c9
FNYukii <forestgreen14689@gmail.com>
2024-02-03 22:54:44 +0900

    feat: 警告に対応

--- src/views/components/sections/ImageFileGrid.tsx
+++ src/views/components/sections/ImageFileGrid.tsx
@@ -11,7 +11,7 @@ function ImageFileGrid(props: Props) {
 
 			{props.images.length === 1 &&
 
-				<img src={window.URL.createObjectURL(props.images[0])} alt="Image for new Spot" className="aspect-square object-cover rounded-xl" />
+				<img src={window.URL.createObjectURL(props.images[0])} alt="For new Spot" className="aspect-square object-cover rounded-xl" />
 			}
 
 
@@ -20,8 +20,8 @@ function ImageFileGrid(props: Props) {
 
 				<div className="grid grid-cols-2 gap-1 aspect-square">
 
-					<img src={window.URL.createObjectURL(props.images[0])} alt="Image for new Spot" className="h-full   object-cover rounded-s-xl" />
-					<img src={window.URL.createObjectURL(props.images[1])} alt="Image for new Spot" className="h-full   object-cover rounded-e-xl" />
+					<img src={window.URL.createObjectURL(props.images[0])} alt="For new Spot" className="h-full   object-cover rounded-s-xl" />
+					<img src={window.URL.createObjectURL(props.images[1])} alt="For new Spot" className="h-full   object-cover rounded-e-xl" />
 				</div>
 			}
 
@@ -31,12 +31,12 @@ function ImageFileGrid(props: Props) {
 
 				<div className="grid grid-cols-2 gap-1   aspect-square">
 
-					<img src={window.URL.createObjectURL(props.images[0])} alt="Image for new Spot" className="h-full   object-cover rounded-s-xl" />
+					<img src={window.URL.createObjectURL(props.images[0])} alt="For new Spot" className="h-full   object-cover rounded-s-xl" />
 
 					<div className="grid grid-rows-2 gap-1">
 
-						<img src={window.URL.createObjectURL(props.images[1])} alt="Image for new Spot" className="aspect-square   object-cover rounded-tr-xl" />
-						<img src={window.URL.createObjectURL(props.images[2])} alt="Image for new Spot" className="aspect-square   object-cover rounded-br-xl" />
+						<img src={window.URL.createObjectURL(props.images[1])} alt="For new Spot" className="aspect-square   object-cover rounded-tr-xl" />
+						<img src={window.URL.createObjectURL(props.images[2])} alt="For new Spot" className="aspect-square   object-cover rounded-br-xl" />
 					</div>
 				</div>
 			}
@@ -47,10 +47,10 @@ function ImageFileGrid(props: Props) {
 
 				<div className="grid grid-cols-2 gap-1">
 
-					<img src={window.URL.createObjectURL(props.images[0])} alt="Image for new Spot" className="aspect-square object-cover rounded-tl-xl" />
-					<img src={window.URL.createObjectURL(props.images[1])} alt="Image for new Spot" className="aspect-square object-cover rounded-tr-xl" />
-					<img src={window.URL.createObjectURL(props.images[2])} alt="Image for new Spot" className="aspect-square object-cover rounded-bl-xl" />
-					<img src={window.URL.createObjectURL(props.images[3])} alt="Image for new Spot" className="aspect-square object-cover rounded-br-xl" />
+					<img src={window.URL.createObjectURL(props.images[0])} alt="For new Spot" className="aspect-square object-cover rounded-tl-xl" />
+					<img src={window.URL.createObjectURL(props.images[1])} alt="For new Spot" className="aspect-square object-cover rounded-tr-xl" />
+					<img src={window.URL.createObjectURL(props.images[2])} alt="For new Spot" className="aspect-square object-cover rounded-bl-xl" />
+					<img src={window.URL.createObjectURL(props.images[3])} alt="For new Spot" className="aspect-square object-cover rounded-br-xl" />
 				</div>
 			}
 		</div>
--- src/views/components/sections/ImageGrid.tsx
+++ src/views/components/sections/ImageGrid.tsx
@@ -11,7 +11,7 @@ function ImageGrid(props: Props) {
 
 			{props.imagesUrls.length === 1 &&
 
-				<img src={props.imagesUrls[0]} alt="Image attached on Spot" className="h-full w-full   aspect-square object-cover   bg-gray-200" />
+				<img src={props.imagesUrls[0]} alt="Attached on Spot" className="h-full w-full   aspect-square object-cover   bg-gray-200" />
 			}
 
 
@@ -20,8 +20,8 @@ function ImageGrid(props: Props) {
 
 				<div className="grid grid-cols-2 gap-2   aspect-square   h-full">
 
-					<img src={props.imagesUrls[0]} alt="Image attached on Spot" className="h-full w-full   object-cover bg-gray-200" />
-					<img src={props.imagesUrls[1]} alt="Image attached on Spot" className="h-full w-full   object-cover bg-gray-200" />
+					<img src={props.imagesUrls[0]} alt="Attached on Spot" className="h-full w-full   object-cover bg-gray-200" />
+					<img src={props.imagesUrls[1]} alt="Attached on Spot" className="h-full w-full   object-cover bg-gray-200" />
 				</div>
 			}
 
@@ -31,12 +31,12 @@ function ImageGrid(props: Props) {
 
 				<div className="grid grid-cols-2 gap-2   aspect-square h-full">
 
-					<img src={props.imagesUrls[0]} alt="Image attached on Spot" className="h-full w-full  object-cover bg-gray-200" />
+					<img src={props.imagesUrls[0]} alt="Attached on Spot" className="h-full w-full  object-cover bg-gray-200" />
 
 					<div className="grid grid-rows-2 gap-2">
 
-						<img src={props.imagesUrls[1]} alt="Image attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
-						<img src={props.imagesUrls[2]} alt="Image attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
+						<img src={props.imagesUrls[1]} alt="Attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
+						<img src={props.imagesUrls[2]} alt="Attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
 					</div>
 				</div>
 			}
@@ -47,10 +47,10 @@ function ImageGrid(props: Props) {
 
 				<div className="grid grid-cols-2 gap-2   aspect-square h-full">
 
-					<img src={props.imagesUrls[0]} alt="Image attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
-					<img src={props.imagesUrls[1]} alt="Image attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
-					<img src={props.imagesUrls[2]} alt="Image attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
-					<img src={props.imagesUrls[3]} alt="Image attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
+					<img src={props.imagesUrls[0]} alt="Attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
+					<img src={props.imagesUrls[1]} alt="Attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
+					<img src={props.imagesUrls[2]} alt="Attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
+					<img src={props.imagesUrls[3]} alt="Attached on Spot" className="w-full h-full aspect-square   object-cover bg-gray-200" />
 				</div>
 			}
 		</div>
