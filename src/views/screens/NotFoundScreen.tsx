57fe91a07edb42e12f2010fdfc684277ca2ec144
FNYukii <forestgreen14689@gmail.com>
2024-02-05 01:25:37 +0900

    feat: NotFoundScreen

--- src/views/screens/NotFoundScreen.tsx
+++ src/views/screens/NotFoundScreen.tsx
@@ -1,10 +1,16 @@
+import { NavLink } from "react-router-dom"
+
 function NotFoundScreen() {
 
 	return (
 
-		<div>
+		<div className="text-center">
 
-			<p>404 Screen here.</p>
+			<p className="mt-16 font-bold text-3xl">Page not found</p>
+			<p className="mt-1">このページは存在しないか、現在のサインイン状態ではアクセスできません</p>
+
+			<NavLink to="/" className="inline-block mt-4   px-6 py-2   bg-black text-white font-bold rounded-full   hover:bg-gray-600 transition">トップへ戻る</NavLink>
+
 		</div>
 	)
 }
