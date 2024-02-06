38e6d59045d2d88cca38a259153411cb52966738
FNYukii <forestgreen14689@gmail.com>
2024-02-06 11:05:50 +0900

    feat: EditUserScreenにUI部品を配置

--- src/views/screens/EditUserScreen.tsx
+++ src/views/screens/EditUserScreen.tsx
@@ -1,19 +1,43 @@
 import Screen from "../components/parts/Screen"
 import FormModal from "../components/parts/FormModal"
-import UserIcon from "../components/parts/UserIcon"
 import AuthService from "../../utils/AuthService"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import LoadingIcon from "../components/parts/LoadingIcon"
+import UserService from "../../utils/UserService"
+import User from "../../entities/User"
 
 
 
 function EditUserScreen() {
 
+	// 入力内容
 	const [displayName, setDisplayName] = useState("")
+
+	// 現在のUser
+	const [user, setUser] = useState<User | null>(null)
+	const [isLoadedUser, setIsLoadedUser] = useState(false)
+
+	useEffect(() => {
+
+		(async () => {
+
+			const user = await UserService.readUser(AuthService.uidQuickly() ?? "---")
+
+			setUser(user)
+			setIsLoadedUser(true)
+
+			if (user) setDisplayName(user.displayName)
+		})()
+	}, [])
+
+
 
 	const [isLoading, setIsLoading] = useState(false)
 
-
+	async function edit() {
+		setIsLoading(true)
+		setIsLoading(false)
+	}
 
 
 
@@ -25,28 +49,41 @@ function EditUserScreen() {
 
 				<div className="w-full sm:w-[400px] ">
 
-					<h1 className="text-2xl font-bold">プロフィールを編集</h1>
+					{!isLoadedUser &&
+						<LoadingIcon  center />
+					}
 
-					<UserIcon userId={AuthService.uidQuickly() ?? "---"} className="mt-4   w-32 mx-auto" />
+					{isLoadedUser && user === null &&
+						<p className="text-center text-gray-500">読み取りに失敗しました</p>
+					}
 
-					<input value={displayName} onChange={e => setDisplayName(e.target.value)} placeholder="ディスプレイネーム" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
+					{isLoadedUser && user !== null &&
 
+						<div>
+							<h1 className="text-2xl font-bold">プロフィールを編集</h1>
 
-					<div className="mt-4   flex justify-end">
+							<img src={user.iconUrl} alt="User icon" className="mt-4 mx-auto   w-32 aspect-square rounded-full   bg-gray/50" />
+							<input value={displayName} onChange={e => setDisplayName(e.target.value)} placeholder="ディスプレイネーム" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
 
-						{!isLoading &&
+							<div className="mt-4   flex justify-end">
 
-							<button
-								className="px-6 py-1   bg-black text-white font-bold rounded-full   disabled:bg-gray-400   enabled:hover:bg-gray-600 transition"
-							>
-								完了
-							</button>
-						}
+								{!isLoading &&
 
-						{isLoading &&
-							<LoadingIcon className="mt-5" color="#000" />
-						}
-					</div>
+									<button
+										onClick={edit}
+										disabled={displayName === ""}
+										className="px-6 py-1   bg-black text-white font-bold rounded-full   disabled:bg-gray-400   enabled:hover:bg-gray-600 transition"
+									>
+										完了
+									</button>
+								}
+
+								{isLoading &&
+									<LoadingIcon className="mt-5" color="#000" />
+								}
+							</div>
+						</div>
+					}
 				</div>
 			</FormModal>
 		</Screen>
