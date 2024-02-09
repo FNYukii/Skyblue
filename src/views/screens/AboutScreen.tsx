421d854a98e9b65b47120c9ceb638313ae90be4c
FNYukii <forestgreen14689@gmail.com>
2024-02-09 11:45:34 +0900

    feat: 使用技術を記載

--- src/views/screens/AboutScreen.tsx
+++ src/views/screens/AboutScreen.tsx
@@ -18,8 +18,14 @@ function AboutScreen() {
 					<p className="mt-8 text-lg font-bold">ソースコード</p>
 					<p className="text-gray-500">このアプリのソースコードは <a target="blank" href="https://github.com/FNYukii/Skyblue" className="hover:underline">GitHub</a> で確認できます。</p>
 					
-					<p className="mt-8 text-lg font-bold">システム概要</p>
-					<p className="text-gray-500">Readme here.</p>
+					<p className="mt-8 text-lg font-bold">主な使用技術</p>
+					<ul className="text-gray-500 list-disc list-inside ml-2">
+						<li>React</li>
+						<li>Tailwind CSS</li>
+						<li>Firebase</li>
+						<li>Google Map Platform</li>
+					</ul>
+					<p className="mt-2 text-gray-500">その他の使用ライブラリは <a target="blank" href="https://github.com/FNYukii/Skyblue" className="hover:underline">ソースコード</a> をご確認ください。</p>
 
 				</div>
 			</div>
