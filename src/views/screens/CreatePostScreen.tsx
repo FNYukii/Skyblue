ef1e4d156fcfa64f8c64c78fbde4dc867c2d550f
FNYukii <forestgreen14689@gmail.com>
2024-02-09 10:44:39 +0900

    feat: FormModalの横幅をpropsで指定

--- src/views/screens/CreatePostScreen.tsx
+++ src/views/screens/CreatePostScreen.tsx
@@ -14,7 +14,7 @@ import Screen from "../components/others/Screen"
 function CreatePostScreen() {
 
 	const [imageFiles, setImageFiles] = useState<File[]>([])
-	const [location, setLocation] = useState<{lat: number, lng: number} | null>(null)
+	const [location, setLocation] = useState<{ lat: number, lng: number } | null>(null)
 
 	const [name, setName] = useState("")
 	const [detail, setDetail] = useState("")
@@ -62,44 +62,42 @@ function CreatePostScreen() {
 
 		<Screen title="新しい投稿 - Skyblue">
 
-			<FormModal>
+			<FormModal className="w-full sm:w-[500px] md:w-[700px]">
 
-				<div className="w-full sm:w-[500px] md:w-[700px]">
+				<h1 className="text-2xl font-bold">新しい投稿</h1>
 
-					<h1 className="text-2xl font-bold">新しい投稿</h1>
+				<div className="mt-4   grid grid-cols-1 sm:grid-cols-2   gap-x-8 gap-y-4">
 
-					<div className="mt-4   grid grid-cols-1 sm:grid-cols-2   gap-x-8 gap-y-4">
-
-						<div>
-							<PickImagesButton onPick={files => setImageFiles(files)} files={imageFiles} />
-						</div>
-
-						<div>
-							<PickLocationButton location={location} onPick={location => setLocation(location)} />
-
-							<input value={name} onChange={e => setName(e.target.value)} placeholder="建物名" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
-							<DynamicTextarea value={detail} onChange={e => setDetail(e.target.value)} placeholder="詳細・感想" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
-						</div>
+					<div>
+						<PickImagesButton onPick={files => setImageFiles(files)} files={imageFiles} />
 					</div>
 
+					<div>
+						<PickLocationButton location={location} onPick={location => setLocation(location)} />
 
-					<div className="mt-4   flex justify-end">
-
-						{!isLoading &&
-
-							<button
-								className="px-6 py-1   bg-black text-white font-bold rounded-full   disabled:bg-gray-400   enabled:hover:bg-gray-600 transition"
-								disabled={imageFiles.length === 0 || location === null || name === "" || name.length > 50 || detail.length > 100}
-								onClick={create}
-							>
-								投稿する
-							</button>
-						}
-
-						{isLoading &&
-							<LoadingIcon className="mt-5" color="#000" />
-						}
+						<input value={name} onChange={e => setName(e.target.value)} placeholder="建物名" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
+						<DynamicTextarea value={detail} onChange={e => setDetail(e.target.value)} placeholder="詳細・感想" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
 					</div>
+				</div>
+				
+
+
+				<div className="mt-4   flex justify-end">
+
+					{!isLoading &&
+
+						<button
+							className="px-6 py-1   bg-black text-white font-bold rounded-full   disabled:bg-gray-400   enabled:hover:bg-gray-600 transition"
+							disabled={imageFiles.length === 0 || location === null || name === "" || name.length > 50 || detail.length > 100}
+							onClick={create}
+						>
+							投稿する
+						</button>
+					}
+
+					{isLoading &&
+						<LoadingIcon className="mt-5" color="#000" />
+					}
 				</div>
 			</FormModal>
 		</Screen>
