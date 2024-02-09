ed7361f54fc80010cdd0e612cc3530a618e74fb7
FNYukii <forestgreen14689@gmail.com>
2024-02-09 10:58:13 +0900

    feat: テキスト校正

--- src/views/screens/FeedbackScreen.tsx
+++ src/views/screens/FeedbackScreen.tsx
@@ -16,7 +16,8 @@ function FeedbackScreen() {
 					<h1 className="text-2xl font-bold">フィードバック</h1>
 
 					<div className="mt-8   flex justify-between items-center gap-2   flex-col sm:flex-row sm:gap-8">
-						<p className="text-gray-500">アプリに関する意見を送信できます。アプリを利用していて感じた問題や機能改善に関する要望などをお寄せください。 いただいたフィードバックへの返信は行っておりません。</p>
+
+						<p className="text-gray-500">アプリに関する意見を送信できます。アプリを利用していて感じた問題や機能改善に関する要望などをお寄せください。いただいたフィードバックへの返信は行っておりません。<br />送信したフィードバックにはユーザーIDが紐づけされます。</p>
 						<NavLinkToModal to="/settings/feedback/new" className="py-2 px-6 border font-bold rounded-full   whitespace-nowrap   hover:bg-gray-100 transition">作成する</NavLinkToModal>
 					</div>
 				</div>
