e1a6dd8fe8182150fbaa10f57957f3823ebbdc77
FNYukii <forestgreen14689@gmail.com>
2024-02-09 11:16:33 +0900

    feat: ページタイトルを設定

--- src/views/components/others/Screen.tsx
+++ src/views/components/others/Screen.tsx
@@ -2,7 +2,7 @@ import { ReactNode } from "react"
 
 interface Props {
 	title: string
-	children: ReactNode
+	children?: ReactNode
 }
 
 
--- src/views/screens/CreateFeedbackScreen.tsx
+++ src/views/screens/CreateFeedbackScreen.tsx
@@ -4,6 +4,7 @@ import DynamicTextarea from "../components/others/DynamicTextarea"
 import LoadingIcon from "../components/others/LoadingIcon"
 import FeedbackService from "../../utils/FeedbackService"
 import { useNavigate } from "react-router-dom"
+import Screen from "../components/others/Screen"
 
 function CreateFeedbackScreen() {
 
@@ -36,44 +37,47 @@ function CreateFeedbackScreen() {
 
 	return (
 
-		<FormModal className="w-full sm:w-[400px]">
+		<Screen title="フィードバックを作成 - Skyblue">
 
-			{!isCreated &&
-				<>
-					<h1 className="text-2xl font-bold">フィードバックを作成</h1>
+			<FormModal className="w-full sm:w-[400px]">
 
-					<input value={title} onChange={e => setTitle(e.target.value)} placeholder="件名" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
-					<DynamicTextarea value={detail} onChange={e => setDetail(e.target.value)} placeholder="詳細" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
+				{!isCreated &&
+					<>
+						<h1 className="text-2xl font-bold">フィードバックを作成</h1>
 
-					<div className="mt-4   flex justify-end">
+						<input value={title} onChange={e => setTitle(e.target.value)} placeholder="件名" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
+						<DynamicTextarea value={detail} onChange={e => setDetail(e.target.value)} placeholder="詳細" className="block   mt-6 w-full pb-2   bg-transparent border-b border-gray-300   focus:outline-none focus:border-blue-500   placeholder:text-gray-400" />
 
-						{!isLoading &&
+						<div className="mt-4   flex justify-end">
 
-							<button
-								onClick={create}
-								disabled={title === "" || detail === ""}
-								className="px-6 py-1   bg-black text-white font-bold rounded-full   disabled:bg-gray-400   enabled:hover:bg-gray-600 transition"
-							>
-								完了
-							</button>
-						}
+							{!isLoading &&
 
-						{isLoading &&
-							<LoadingIcon className="mt-5" color="#000" />
-						}
+								<button
+									onClick={create}
+									disabled={title === "" || detail === ""}
+									className="px-6 py-1   bg-black text-white font-bold rounded-full   disabled:bg-gray-400   enabled:hover:bg-gray-600 transition"
+								>
+									完了
+								</button>
+							}
+
+							{isLoading &&
+								<LoadingIcon className="mt-5" color="#000" />
+							}
+						</div>
+					</>
+				}
+
+				{isCreated &&
+					<div>
+						<p className="text-2xl font-bold text-center">ありがとうございました</p>
+						<p className="mt-2 text-center">いただいた意見は今後の開発の参考にさせていただきます。</p>
+
+						<button onClick={() => navigate(-1)} className="block mx-auto mt-4   px-6 py-1   bg-black text-white font-bold rounded-full   disabled:bg-gray-400   enabled:hover:bg-gray-600 transition">戻る</button>
 					</div>
-				</>
-			}
-
-			{isCreated &&
-				<div>
-					<p className="text-2xl font-bold text-center">ありがとうございました</p>
-					<p className="mt-2 text-center">いただいた意見は今後の開発の参考にさせていただきます。</p>
-
-					<button onClick={() => navigate(-1)} className="block mx-auto mt-4   px-6 py-1   bg-black text-white font-bold rounded-full   disabled:bg-gray-400   enabled:hover:bg-gray-600 transition">戻る</button>
-				</div>
-			}
-		</FormModal>
+				}
+			</FormModal>
+		</Screen>
 	)
 }
 
