87a92a4f4e4ec40fbc2c275387170dbf9b105818
FNYukii <forestgreen14689@gmail.com>
2024-02-11 14:27:39 +0900

    feat: AboutScreen: スタイル改善

--- src/views/screens/AboutScreen.tsx
+++ src/views/screens/AboutScreen.tsx
@@ -16,16 +16,16 @@ function AboutScreen() {
 					<p className="text-2xl font-bold">このアプリについて</p>
 
 					<p className="mt-8 text-lg font-bold">ソースコード</p>
-					<p className="text-gray-500">このアプリのソースコードは <a target="blank" href="https://github.com/FNYukii/Skyblue" className="hover:underline">GitHub</a> で確認できます。</p>
+					<p className="text-gray-500">このアプリのソースコードは<a target="blank" href="https://github.com/FNYukii/Skyblue" className="text-black hover:underline">GitHub</a>で確認できます。</p>
 					
 					<p className="mt-8 text-lg font-bold">主な使用技術</p>
 					<ul className="text-gray-500 list-disc list-inside ml-2">
 						<li>React</li>
 						<li>Tailwind CSS</li>
 						<li>Firebase</li>
-						<li>Google Map Platform</li>
+						<li>Google Maps Platform</li>
 					</ul>
-					<p className="mt-2 text-gray-500">その他の使用ライブラリは <a target="blank" href="https://github.com/FNYukii/Skyblue" className="hover:underline">ソースコード</a> をご確認ください。</p>
+					<p className="mt-2 text-gray-500">その他の使用ライブラリは<a target="blank" href="https://github.com/FNYukii/Skyblue" className="text-black hover:underline">ソースコード</a>をご確認ください。</p>
 
 				</div>
 			</div>
